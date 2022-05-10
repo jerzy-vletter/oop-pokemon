c5db7a15d03d7655ef79231e8ac4335930ce81e7
J. vletter <99052114@mydavinci.nl>
2022-05-10 11:03:41 +0200

    fixed a gap in the docBlock documentation

--- classes/Attack.php
+++ classes/Attack.php
@@ -13,6 +13,7 @@ class Attack{
 
     /**
      * this constructor breaks up the given data from the pokemon that calls for one of these values so it can be used
+     * 
      *
      * asigns the broken up data to a variable so it can be used in both the attack functionality 
      * and as part of the battleText function.
@@ -28,6 +29,7 @@ class Attack{
 
     /**
      * toString function that turns all data provided above into a string so that they can be used in echo's without to change the dataType when echoing.
+     * 
      *
      * @return string returns a string when called for so that the requested data can be used where needed.
      */
